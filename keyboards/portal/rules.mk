dd6804daa26cb6eed5be12c9f6d255f2a455bf3b
Shuozhe Nan <shuozhe.nan@live.com>
2020-02-22 01:46:43 +0100

    added oled (that was kinda very easy)

--- keyboards/portal/rules.mk
+++ keyboards/portal/rules.mk
@@ -42,3 +42,4 @@ FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 
 #SPLIT_KEYBOARD = yes
+OLED_DRIVER_ENABLE = yes
