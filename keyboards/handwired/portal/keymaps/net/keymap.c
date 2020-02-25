8e57c37f5ed2422c5609d1348db28b8b8546acb2
net <net.ad.net@googlemail.com>
2020-02-25 10:31:25 +0100

    moved my config to handwired subfolder, corrected LAYOUT macro

--- keyboards/handwired/portal/keymaps/net/keymap.c
+++ keyboards/handwired/portal/keymaps/net/keymap.c
@@ -27,11 +27,11 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT(
-        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,           KC_1, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,           KC_2, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-        KC_NUBS, KC_A, KC_S, KC_D, KC_F, KC_G,          KC_3, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_ENT,  KC_4, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                          KC_LCTL, MO(_RAISE), KC_SPC,  KC_5, RAISE,   KC_BSPC, KC_DOWN, KC_LEFT, KC_RIGHT, KC_UP \
+        KC_GRV,  KC_1, KC_2, KC_3, KC_4,    KC_5,                          KC_1,  KC_6,    KC_7,    KC_8,     KC_9,     KC_0,    KC_GRV, \
+        KC_TAB,  KC_Q, KC_W, KC_E, KC_R,    KC_T,                          KC_2,  KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,    KC_MINS, \
+        KC_NUBS, KC_A, KC_S, KC_D, KC_F,    KC_G,                          KC_3,  KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN, KC_QUOT, \
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V,    KC_B,    KC_ENT,      KC_BSPC, KC_N,  KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT, \
+                                   KC_LCTL, KC_LALT, KC_SPC,      KC_SPC,  RAISE, KC_BSPC,          KC_RIGHT, KC_UP,    KC_TILDE \
         ),
 
     [_RAISE] = LAYOUT(
@@ -40,30 +40,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_PGDN,           KC_3, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
         _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, _______,  KC_4, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
         KC_LGUI, _______, _______,                                      KC_5, RAISE,   KC_BSPC, KC_DOWN, KC_LEFT, KC_RIGHT, KC_UP \
-        )};
+        ) };
 
 #ifdef OLED_DRIVER_ENABLE
-static void render_logo(void) {
-    static const char PROGMEM qmk_logo[] = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94, 0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00};
+static void render_logo(void)
+{
+    static const char PROGMEM qmk_logo[] = { 0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94, 0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00 };
 
     oled_write_P(qmk_logo, false);
 }
 
-void oled_task_user(void) {
+void oled_task_user(void)
+{
     // Host Keyboard Layer Status
     oled_write_P(PSTR("Layer: "), false);
 
-    switch (get_highest_layer(layer_state)) {
-        case _QWERTY:
-            //oled_write_P(PSTR("Default\n"), false);
-            render_logo();
-            break;
-        case _RAISE: 
-            oled_write_P(PSTR("RAISE\n"), false);
-            break;
-        default:
-            // Or use the write_ln shortcut over adding '\n' to the end of your string
-            oled_write_ln_P(PSTR("Undefined"), false);
+    switch (get_highest_layer(layer_state))
+    {
+    case _QWERTY:
+        //oled_write_P(PSTR("Default\n"), false);
+        render_logo();
+        break;
+    case _RAISE:
+        oled_write_P(PSTR("RAISE\n"), false);
+        break;
+    default:
+        // Or use the write_ln shortcut over adding '\n' to the end of your string
+        oled_write_ln_P(PSTR("Undefined"), false);
     }
 
     // Host Keyboard LED Status
