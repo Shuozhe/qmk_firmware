4aea55269a33ff29edb564e2bfffd60b25eac110
net <net.ad.net@googlemail.com>
2020-02-26 16:36:32 +0100

    start doing some oled stuff, need to connect both side via serial instead of i2c

--- keyboards/handwired/portal/portal.h
+++ keyboards/handwired/portal/portal.h
@@ -18,30 +18,33 @@
 
 #include "quantum.h"
 
-/* This is a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
- */
+ /* This is a shortcut to help you visually see your layout.
+  *
+  * The first section contains all of the arguments representing the physical
+  * layout of the board and position of the keys.
+  *
+  * The second converts the arguments into a two-dimensional array which
+  * represents the switch matrix.
+  */
 #define LAYOUT( \
-    LA1, LA2, LA3, LA4, LA5, LA6,             RA7, RA6, RA5, RA4, RA3, RA2, RA1, \
-    LB1, LB2, LB3, LB4, LB5, LB6,             RB7, RB6, RB5, RB4, RB3, RB2, RB1, \
-    LC1, LC2, LC3, LC4, LC5, LC6,             RC7, RC6, RC5, RC4, RC3, RC2, RC1, \
-    LD1, LD2, LD3, LD4, LD5, LD6, LE6,   RE7, RD7, RD6, RD5, RD4, RD3, RD2, RD1, \
-                        LE3, LE4, LE5,   RE6, RE5, RE4, RE3, RE2, RE1 \
+    LA1, LA2, LA3, LA4, LA5, LA6, \
+    LB1, LB2, LB3, LB4, LB5, LB6, \
+    LC1, LC2, LC3, LC4, LC5, LC6, \
+    LD1, LD2, LD3, LD4, LD5, LD6, LE6, \
+                        LE3, LE4, LE5, \
+         RA7, RA6, RA5, RA4, RA3, RA2, RA1, \
+         RB7, RB6, RB5, RB4, RB3, RB2, RB1, \
+         RC7, RC6, RC5, RC4, RC3, RC2, RC1, \
+    RE7, RD7, RD6, RD5, RD4, RD3, RD2, RD1, \
+    RE6, RE5, RE4, RE3, RE2, RE1 \
     ) \
-    { \
-        { KC_NO, LA1, LA2, LA3, LA4, LA5, LA6}, \
-        { KC_NO, LB1, LB2, LB3, LB4, LB5, LB6}, \
-        { KC_NO, LC1, LC2, LC3, LC4, LC5, LC6}, \
-        { KC_NO, LD1, LD2, LD3, LD4, LD5, LD6}, \
-        { KC_NO, KC_NO, KC_NO, LE5, LE3, LE4, LE6 }, \
-        { RA1, RA2, RA3, RA4, RA5, RA6, RA7 }, \
-        { RB1, RB2, RB3, RB4, RB5, RB6, RB7 }, \
-        { RC1, RC2, RC3, RC4, RC5, RC6, RC7 }, \
-        { RD1, RD2, RD3, RD4, RD5, RD6, RD7 }, \
-        { RE1, RE2, RE3, RE4, RE5, RE6, RE7 } \
-    }
+    {{ KC_NO, LA1,   LA2,   LA3, LA4, LA5, LA6}, \
+     { KC_NO, LB1,   LB2,   LB3, LB4, LB5, LB6}, \
+     { KC_NO, LC1,   LC2,   LC3, LC4, LC5, LC6}, \
+     { KC_NO, LD1,   LD2,   LD3, LD4, LD5, LD6}, \
+     { KC_NO, KC_NO, KC_NO, LE5, LE3, LE4, LE6 }, \
+     { RA1,   RA2,   RA3,   RA4, RA5, RA6, RA7 }, \
+     { RB1,   RB2,   RB3,   RB4, RB5, RB6, RB7 }, \
+     { RC1,   RC2,   RC3,   RC4, RC5, RC6, RC7 }, \
+     { RD1,   RD2,   RD3,   RD4, RD5, RD6, RD7 }, \
+     { RE1,   RE2,   RE3,   RE4, RE5, RE6, RE7 }}
