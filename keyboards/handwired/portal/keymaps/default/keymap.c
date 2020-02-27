1c693458f9cadf429c6c328a7693c02cb25b8c5b
net <net.ad.net@googlemail.com>
2020-02-27 17:29:29 +0100

    little update to layout, added all planed layer back in

--- keyboards/handwired/portal/keymaps/default/keymap.c
+++ keyboards/handwired/portal/keymaps/default/keymap.c
@@ -19,7 +19,6 @@
 
 extern keymap_config_t keymap_config;
 
-#define LA(KC) LT(0, KC)
 #define _QWERTY 0
 #define _RAISE 1
 #define _CALCULATOR 2
@@ -38,41 +37,49 @@ enum custom_keycodes {
     CCCV
 };
 
+#define L0(KC) LT(0, KC)
+#define L1(KC) LT(1, KC)
+#define L2(KC) LT(2, KC)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT(
       KC_GESC,   KC_1,   KC_2,   KC_3,    KC_4,    KC_5,                   \
       KC_TAB,    KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                   \
       KC_LSFT,   KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                   \
-      KC_LCTL,   KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,    LT(1, KC_ENT), \
+      KC_LCTL,   KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,    L1(KC_ENT),    \
                                           KC_LALT, KC_LGUI, KC_SPC,        \
 \
-                 CCCV,      KC_6,    KC_7,   KC_8,    KC_9,    KC_0,     KC_EQUAL,         \
-                 KC_LGUI,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,     KC_MINS,          \
-                 RANDOM,    KC_H,    KC_J,   KC_K,    KC_L,    KC_LBRC,  KC_RBRC,          \
-      KC_SCLN,   KC_QUOT,   KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  RSFT_T(KC_CAPS), \
-      KC_BSPC,   TT(3),     KC_BSPC,                  KC_LEFT, KC_RIGHT, KC_DOWN),
+                    CCCV,      KC_6,    KC_7,   KC_8,    KC_9,    KC_0,     KC_EQUAL, \
+                    KC_PLUS,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,     KC_MINS,  \
+                    RANDOM,    KC_H,    KC_J,   KC_K,    KC_L,    KC_LBRC,  KC_RBRC,  \
+      L2(KC_SCLN),  KC_QUOT,   KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_TOP,   KC_SLSH,  \
+      KC_BSPC,      TT(3),     KC_BSPC,                  KC_LEFT, KC_DOWN, KC_RIGHT),
 
     [_RAISE] = LAYOUT(
       LA(KC_F12), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, \
       KC_INS, KC_HOME, KC_UP, KC_END, _______, KC_PGUP, \
         _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_PGDN, \
-        _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, KC_TRNS, \
-                                            KC_LGUI, _______, KC_TRNS, \
+        _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, _______, \
+                                            KC_LGUI, _______, _______, \
 \
-          KC_F6, KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12, \
-          KC_2,  KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-          KC_3,  KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-      KC_BSPC, KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-      KC_SPC, KC_TRNS, KC_BSPC, KC_DOWN, KC_LEFT, KC_RIGHT, KC_UP),
+                    CCCV,      KC_6,    KC_7,   KC_8,    KC_9,    KC_0,     KC_EQUAL, \
+                    KC_PLUS,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,     KC_MINS,  \
+                    RANDOM,    KC_H,    KC_J,   KC_K,    KC_L,    KC_LBRC,  KC_RBRC,  \
+      L2(KC_SCLN),  KC_QUOT,   KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_TOP,   KC_SLSH,  \
+      KC_BSPC,      TT(3),     KC_BSPC,                  KC_LEFT, KC_DOWN, KC_RIGHT),
 
-    /* [_CALCULATOR] = LAYOUT(
-         LA(KC_KP_SLASH),  KC_KP_7, KC_KP_8,     KC_KP_9,   KC_KP_0, KC_F5,                      KC_1, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
-            KC_KP_ASTERIX, KC_KP_4, KC_KP_5,     KC_KP_6,   LSFT(KC_5), KC_PGUP,               KC_2, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-            KC_KP_MINUS,   KC_KP_1, KC_KP_2,     KC_KP_3,   KC_KP_EQUAL, KC_PGDN,           KC_3, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-            KC_KP_PLUS,    KC_KP_0, KC_KP_COMMA, KC_KP_DOT, KC_KP_ENTER, _______, KC_TRNS,  KC_4, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                                                            KC_LGUI, _______, KC_TRNS,                                      KC_5, RAISE,   KC_BSPC, KC_DOWN, KC_LEFT, KC_RIGHT, KC_UP \
-            )*/
+    [_CALCULATOR] = LAYOUT(
+        LA(KC_KP_SLASH),  KC_KP_7, KC_KP_8,  KC_KP_9,   KC_KP_0,     KC_F5, \
+        KC_KP_ASTERIX, KC_KP_4, KC_KP_5,     KC_KP_6,   LSFT(KC_5),  KC_PGUP, \
+        KC_KP_MINUS,   KC_KP_1, KC_KP_2,     KC_KP_3,   KC_KP_EQUAL, KC_PGDN, \
+        KC_KP_PLUS,    KC_KP_0, KC_KP_COMMA, KC_KP_DOT, KC_KP_ENTER, _______, _______, \
+                                                        KC_LGUI,     _______, _______, \
+\
+                    CCCV,      KC_6,    KC_7,   KC_8,    KC_9,    KC_0,     KC_EQUAL, \
+                    KC_PLUS,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,     KC_MINS,  \
+                    RANDOM,    KC_H,    KC_J,   KC_K,    KC_L,    KC_LBRC,  KC_RBRC,  \
+      L2(KC_SCLN),  KC_QUOT,   KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_TOP,   KC_SLSH,  \
+      KC_BSPC,      TT(3),     KC_BSPC,                  KC_LEFT, KC_DOWN, KC_RIGHT),
 
     [_COLEMAK] = LAYOUT(
       KC_GESC, KC_1, KC_2, KC_3, KC_4,    KC_5, \
@@ -174,5 +181,5 @@ void oled_task_user(void)
     oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
     oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
     oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
-}
+    }
 #endif
